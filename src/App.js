24173c575f47cd61d17fb8e327cc43c512a7694c
radityanatha <radityanatha20@gmail.com>
2025-05-29 05:24:17 +0800

    add stack-navigator and fixing

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import { NavigationContainer } from '@react-navigation/native'
 import { Splashscreen } from './assets/pages'
 import 'react-native-gesture-handler'
+import Router from './router'
 
 export default function App() {
   return (
     <NavigationContainer>
-      <Splashscreen />
+      <Router />
     </NavigationContainer>
   )
 }
--- src/router/index.js
+++ src/router/index.js
@@ -2,14 +2,15 @@ import { StyleSheet, Text, View } from 'react-native'
 import React from 'react'
 import { createStackNavigator } from '@react-navigation/stack'
 import { Splashscreen } from '../assets/pages'
+import Home from '../assets/pages/Home'
 
 const Stack = createStackNavigator()
 
 const Router = () => {
   return (
-    <Stack.Navigator>
-      <Stack.Screen name="Home" component={Splashscreen} />
-      <Stack.Screen name="Notifications" component={Home} />
+    <Stack.Navigator screenOptions={{ headerShown: false }}>
+      <Stack.Screen name="Splashscreen" component={Splashscreen} />
+      <Stack.Screen name="Home" component={Home} />
     </Stack.Navigator>
   )
 }
