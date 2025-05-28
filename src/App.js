96e6b3ef7e583bb5e04fa67805783036255ce4f2
radityanatha <radityanatha20@gmail.com>
2025-05-29 04:45:06 +0800

    first commit - add stack navigator

---
+++ src/App.js
@@ -0,0 +1,10 @@
+import { NavigationContainer } from '@react-navigation/native'
+import { Splashscreen } from './assets/pages'
+
+export default function App() {
+  return (
+    <NavigationContainer>
+      <Splashscreen />
+    </NavigationContainer>
+  )
+}
---
+++ src/assets/pages/Splashscreen/index.js
@@ -0,0 +1,14 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+
+const Splashscreen = () => {
+  return (
+    <View style={styles.container}>
+      <Text>Splashscreen</Text>
+    </View>
+  )
+}
+
+export default Splashscreen
+
+const styles = StyleSheet.create({})
